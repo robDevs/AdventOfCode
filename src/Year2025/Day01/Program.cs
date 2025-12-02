9a4ecd9a65958d87f2c4752b15ee231a319d31e4
robDevs <robbdevv@gmail.com>
2025-12-01 20:55:41 -0700

    solve day01 part02

--- src/Year2025/Day01/Program.cs
+++ src/Year2025/Day01/Program.cs
@@ -1,6 +1,7 @@
 ﻿string[] lines = File.ReadAllLines("src/Year2025/Day01/input.txt");
 
-Console.WriteLine($"Solving Day01: {SolvePartOne(lines)}");
+Console.WriteLine($"Solving Day01 Part01: {SolvePartOne(lines)}");
+Console.WriteLine($"Solving Day01 Part02: {SolvePartTwo(lines)}");
 
 static int SolvePartOne(string[] lines)
 {
@@ -32,6 +33,39 @@ static int SolvePartOne(string[] lines)
         if (position == 0)
         {
             count += 1;
+        }
+    }
+
+    return count;
+}
+
+static int SolvePartTwo(string[] lines)
+{
+    int count = 0;
+    int position = 50; // start at 50, range = 0-99
+
+    // this time we want to count how many times the dial points at 0, not just when it lands on 0;
+
+    foreach (var line in lines)
+    {
+        var direction = line.Substring(0, 1);
+        int steps = int.Parse(line.Substring(1));
+
+        for (int i = 0; i < steps; i++)
+        {
+            if (direction.Equals("L"))
+            {
+                position -= 1;
+            }
+            else
+            {
+                position += 1;
+            }
+
+            if (position < 0) position = 99;
+            if (position > 99) position = 0;
+
+            if (position == 0) count++;
         }
     }
 
