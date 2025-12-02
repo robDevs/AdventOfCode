a84c2b344125b4e07a0802e57add4a18679fe7e7
robDevs <robbdevv@gmail.com>
2025-12-01 20:40:56 -0700

    solve day01 part01

--- src/Year2025/Day01/Program.cs
+++ src/Year2025/Day01/Program.cs
@@ -1,8 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-GetNumber(0);
+﻿string[] lines = File.ReadAllLines("src/Year2025/Day01/input.txt");
 
-static int GetNumber(int number)
+Console.WriteLine($"Solving Day01: {SolvePartOne(lines)}");
+
+static int SolvePartOne(string[] lines)
 {
-    return number;
+    int count = 0; // count how many times the dial lands on 0;
+    int position = 50; // current dial position, starts at 50. Range = 0-99
+
+    foreach (var line in lines)
+    {
+        var direction = line.Substring(0, 1);
+        int steps = int.Parse(line.Substring(1));
+
+        if (direction.Equals("L")) // L means turn left, R means turn right
+        {
+            steps *= -1;
+        }
+
+        position += steps;
+
+        // steps could be greater than the size of the "wheel" 
+        // so we adjust by the size of the wheel until it's whithin range. 
+        while (position < 0)
+        {
+            position = 100 - Math.Abs(position);
+        }
+        while (position > 99)
+        {
+            position -= 100;
+        }
+        if (position == 0)
+        {
+            count += 1;
+        }
+    }
+
+    return count;
 }
