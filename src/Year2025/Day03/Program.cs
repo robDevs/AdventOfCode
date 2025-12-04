649151392297bac9492a8825e742255f5b20de1d
robDevs <robbdevv@gmail.com>
2025-12-03 20:57:37 -0700

    day03 part01

---
+++ src/Year2025/Day03/Program.cs
@@ -0,0 +1,29 @@
+﻿
+string[] lines = File.ReadAllLines("src/Year2025/Day03/input.txt");
+
+Console.WriteLine($"Solving part one: {SolvePartOne(lines)}");
+
+static int SolvePartOne(string[] input)
+{
+    int sumLargestPairs = 0;
+
+    foreach (string line in input)
+    {
+        int largest = 0;
+        for (int i = 0; i < line.Length - 1; i++)
+        {
+            for (int x = i + 1; x < line.Length; x++)
+            {
+                int pair = int.Parse(line[i].ToString() + line[x].ToString());
+
+                Console.WriteLine(line[i].ToString() + line[x].ToString());
+                Console.WriteLine(pair);
+
+                if (pair > largest) largest = pair;
+            }
+        }
+        sumLargestPairs += largest;
+    }
+
+    return sumLargestPairs;
+}
