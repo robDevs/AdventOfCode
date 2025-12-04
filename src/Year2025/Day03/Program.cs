db5bb56f62fd1f9261ba5a2b95617151a13274d4
robDevs <robbdevv@gmail.com>
2025-12-03 22:46:27 -0700

    day03 part02

--- src/Year2025/Day03/Program.cs
+++ src/Year2025/Day03/Program.cs
@@ -2,7 +2,10 @@
 string[] lines = File.ReadAllLines("src/Year2025/Day03/input.txt");
 
 Console.WriteLine($"Solving part one: {SolvePartOne(lines)}");
+Console.WriteLine($"Solving part two: {SolvePartTwo(lines)}");
 
+// find the largest ordered pair on each line. 
+// add it to the sum. return sum
 static int SolvePartOne(string[] input)
 {
     int sumLargestPairs = 0;
@@ -12,6 +15,42 @@ static int SolvePartOne(string[] input)
         int first = LargestIntPos(line.Substring(0, line.Length - 1));
         int second = first + 1 + LargestIntPos(line.Substring(first + 1));
         sumLargestPairs += int.Parse(line[first].ToString() + line[second].ToString());
+    }
+
+    return sumLargestPairs;
+}
+
+// same as part 1 but with an ordered sequence 12 digits long. 
+// find the largest possible ordered sequence in each line
+// then add it to the sum. 
+static long SolvePartTwo(string[] input)
+{
+    long sumLargestPairs = 0;
+
+    foreach (string line in input)
+    {
+        List<int> posList = [];
+
+        for (int i = 0; i < 12; i++)
+        {
+            if (i == 0)
+            {
+                posList.Add(LargestIntPos(line.Substring(0, line.Length - 11)));
+                continue;
+            }
+
+            string sub = line.Substring(posList.Last() + 1);
+            string searchArea = sub.Substring(0, sub.Length - (11 - posList.Count()));
+            posList.Add(posList.Last() + 1 + LargestIntPos(searchArea));
+        }
+
+        string final = "";
+        foreach (var item in posList)
+        {
+            final += line[item];
+        }
+
+        sumLargestPairs += long.Parse(final);
     }
 
     return sumLargestPairs;
