56fc897f75e718fb5452f095424b50fb6458f6cf
robDevs <robbdevv@gmail.com>
2025-12-06 23:21:58 -0700

    day05 part02

--- src/Year2025/Day05/Program.cs
+++ src/Year2025/Day05/Program.cs
@@ -1,6 +1,7 @@
 ﻿string[] lines = File.ReadAllLines("src/Year2025/Day05/input.txt");
 
 Console.WriteLine($"Solving Day05 part01 {SolvePartOne(lines)}");
+Console.WriteLine($"Solving Day05 part02 {SolvePartTwo(lines)}");
 
 static int SolvePartOne(string[] lines)
 {
@@ -44,6 +45,96 @@ static int SolvePartOne(string[] lines)
             }
         }
         if (fresh) continue;
+    }
+
+    return sum;
+}
+
+static long SolvePartTwo(string[] lines)
+{
+    List<Tuple<long, long>> ranges = [];
+    List<Tuple<long, long>> combinedRanges = [];
+
+    foreach (var line in lines)
+    {
+        if (line == string.Empty)
+        {
+            break;
+        }
+
+        var start = long.Parse(line.Split('-')[0]);
+        var end = long.Parse(line.Split('-')[1]);
+
+        ranges.Add(new Tuple<long, long>(start, end));
+    }
+
+    ranges.Sort((a, b) => a.Item1.CompareTo(b.Item1));
+
+    foreach (var range in ranges)
+    {
+        long start = range.Item1;
+        long end = range.Item2;
+
+        if (combinedRanges.Count == 0)
+        {
+            combinedRanges.Add(new Tuple<long, long>(start, end));
+            continue;
+        }
+
+        long? newStart = null;
+        long? newEnd = null;
+
+        long removeStart = 0;
+        long removeEnd = 0;
+
+        foreach (var compRange in combinedRanges) // the range to compare to
+        {
+            long compStart = compRange.Item1;
+            long compEnd = compRange.Item2;
+
+            if (start <= compEnd && end >= compStart)
+            {
+                removeStart = compStart;
+                removeEnd = compEnd;
+                if (start < compStart)
+                {
+                    newStart = start;
+                }
+                else
+                {
+                    newStart = compStart;
+                }
+
+                if (end > compEnd)
+                {
+                    newEnd = end;
+                }
+                else
+                {
+                    newEnd = compEnd;
+                }
+                break;
+            }
+        }
+
+        if (newStart != null)
+        {
+            var toRemove = combinedRanges.First(r => r.Item1 == removeStart && r.Item2 == removeEnd);
+            combinedRanges.Remove(toRemove);
+            combinedRanges.Add(new Tuple<long, long>((long)newStart, (long)newEnd!));
+        }
+        else
+        {
+            combinedRanges.Add(range);
+        }
+    }
+
+    long sum = 0;
+
+    foreach (var range in combinedRanges)
+    {
+        sum += range.Item2 - range.Item1;
+        sum += 1;
     }
 
     return sum;
